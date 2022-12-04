e2f0a2e2fad36a9f7a5934a4f17a07bd437cd0b7
Faran Ahmad Khan <73027299+faranak-cs@users.noreply.github.com>
2022-12-04 16:45:15 +0500

    Main application

--- Dictionary.cpp
+++ Dictionary.cpp
@@ -1,22 +1,4 @@
-/* Digital Dictionary via AVL & Trie trees
- * Project (C++ console based)
- * Course:
- *      ~ CS-250 Data Structures & Algorithms
- * Group Member(s):
- *      ~ Faran - 333858
- *      ~ Haseeb - 342608
- * Course Instructor:
- *     ~ Dr. Yasir Faheem
- * Description:
- *      ~ This is a C++ console based project that allows
- *      ~ the user to search a word just like an Online Dictionary.
- *      ~ Application will return its meaning (synonym), part of speech,
- *      ~ and history of word (if any). The user can compare between
- *      ~ the Data Structures used (AVL Tree, Trie Tree) which is faster.
- */
-
-
- //libraries are included
+//libraries are included
 
 #include <iostream>
 #include <fstream>
